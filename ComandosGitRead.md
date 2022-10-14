9990f265e465cc10f6de21a36583f651a5707ab1
d6pra <contatotheonegamerbr@gmail.com>
2022-10-13 21:31:06 -0300

    console log atualizado

--- ComandosGitRead.md
+++ ComandosGitRead.md
@@ -30,4 +30,6 @@ git add .
 
 git commit -m "commit"
 
+git push -u origin main
+
 https://www.youtube.com/watch?v=UBAX-13g8OM
--- index.js
+++ index.js
@@ -1,1 +1,67 @@
-console.log('Usando Server Manager V14-D6pra')
+const time1 = setTimeout(a1, 5000);
+
+function a1() {
+    console.log(`...`)
+  }
+  const time2 = setTimeout(a2, 5000);
+
+  function a2() {
+    console.log(`🔥 Server Manager | V14`)
+  }
+  const time3 = setTimeout(a3, 5000);
+  function a3() {
+    console.log(`📃 API / Discord carregada!`)
+  }
+  const time4 = setTimeout(a4, 5000);
+  
+  function a4() {
+    console.log(`❌ Square Cloud Host = false`)
+  }
+
+  const time5 = setTimeout(a5, 5000);
+
+  function a5() {
+    console.log(`📁 Pasta "Comandos" carregada`)
+  }
+
+
+  const time6 = setTimeout(a6, 5000);
+ 
+  function a6() {
+    console.log(`📁 Pasta "adm" carregada`)
+  }
+  const time7 = setTimeout(a7, 5000);
+ 
+  function a7() {
+    console.log(`📁 Pasta "all" carregada`)
+  }
+  const time8 = setTimeout(a8, 5000);
+
+  function a8() {
+    console.log(`📁 Pasta "handler" carregada`)
+  }
+  const time9 = setTimeout(a9, 5000);
+
+  function a9() {
+    console.log(`📁 Pasta "node_modules" carregada`)
+  }
+  const time10 = setTimeout(a10, 5000);
+
+  function a10() {
+    console.log(`📄 DataBase / json.sqlite carregada`)
+  }
+  const time11 = setTimeout(a11, 5000);
+
+  function a11() {
+    console.log(`📄 Base / BASE.js carregada`)
+  }
+  const time12 = setTimeout(a12, 5000);
+
+  function a12() {
+    console.log(`🔥 Server Manager | V14`) 
+  }
+  const time13 = setTimeout(a13, 5000);
+  function a13() {
+    console.log(`✅ Estou online`)
+  }
+ 
